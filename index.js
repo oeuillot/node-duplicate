1612da5bb2b4e2e128b373bb2cb52f4c4512ff30
oeuillot <olivier.oeuillot@gmail.com>
2014-08-12 17:23:08 +0200

    Fix when the resource is not readable

--- index.js
+++ index.js
@@ -10,114 +10,123 @@ var filesCount = 0;
 var maxHashSize = 1024 * 1024;
 
 var filesSHA1 = Async.queue(function(path, callback) {
-  // console.log("SHA1 of " + path);
-  var shasum = crypto.createHash('sha256');
-  var max = program.maxHashSize || maxHashSize;
+	// console.log("SHA1 of " + path);
+	var shasum = crypto.createHash('sha256');
+	var max = program.maxHashSize || maxHashSize;
 
-  var count = 0;
-  var s = fs.ReadStream(path);
-  var closed = false;
+	var count = 0;
+	var s = fs.ReadStream(path);
+	var closed = false;
 
-  function end() {
-    if (closed) {
-      return;
-    }
-    closed = true;
+	function end() {
+		if (closed) {
+			return;
+		}
+		closed = true;
 
-    s.close();
+		s.close();
 
-    var d = shasum.digest('base64');
+		var d = shasum.digest('base64');
 
-    fs.lstat(path, function(error, stats) {
-      if (error) {
-        return callback(error);
-      }
-      // console.log("SHA1Stat " + path + " => " + filesRepository[stats.size]);
-      var g = filesRepository[stats.size][d];
-      if (!g) {
-        g = [];
-        filesRepository[stats.size][d] = g;
-      }
-      g.push(path);
+		fs.lstat(path, function(error, stats) {
+			if (error) {
+				return callback(error);
+			}
+			// console.log("SHA1Stat " + path + " => " + filesRepository[stats.size]);
+			var g = filesRepository[stats.size][d];
+			if (!g) {
+				g = [];
+				filesRepository[stats.size][d] = g;
+			}
+			g.push(path);
 
-      return callback(null);
-    });
-  }
+			return callback(null);
+		});
+	}
 
-  s.on('data', function(d) {
-    if (closed) {
-      return;
-    }
-    count += d.length;
+	s.on('data', function(d) {
+		if (closed) {
+			return;
+		}
+		count += d.length;
 
-    shasum.update(d);
+		shasum.update(d);
 
-    if (max > 0 && count > max) {
-      end();
-    }
-  });
+		if (max > 0 && count > max) {
+			end();
+		}
+	});
 
-  s.on('end', end);
+	s.on('end', end);
 
 }, 1);
 
 var directories = Async.queue(function(directory, callback) {
 
-  console.log("Scan directory " + directory);
-  fs.readdir(directory, function(error, files) {
-    if (error) {
-      return callback(error);
-    }
+	console.log("Scan directory " + directory);
+	fs.readdir(directory, function(error, files) {
+		if (error) {
+			if (error.code === "EACCES") {
+				console.log("Can not access to directory: " + directory);
+				return callback(null);
+			}
 
-    Async.forEach(files, function(file, callback) {
+			return callback(error);
+		}
 
-      var path = directory + Path.sep + file;
+		Async.forEach(files, function(file, callback) {
 
-      fs.lstat(path, function(error, stats) {
+			var path = directory + Path.sep + file;
 
-        // console.log("Stat " + file + " " + stats);
-        if (error) {
-          return callback(error);
-        }
+			fs.lstat(path, function(error, stats) {
 
-        if (stats.isDirectory()) {
-          // console.log("Add directories " + file);
-          directories.push(path);
+				// console.log("Stat " + file + " " + stats);
+				if (error) {
+					if (error.code === "EACCES") {
+						console.log("Can not access to file: " + path);
+						return callback(null);
+					}
+					return callback(error);
+				}
 
-        } else if (stats.isFile() && stats.size) {
-          // console.log("Stat file " + file);
-          filesCount++;
+				if (stats.isDirectory()) {
+					// console.log("Add directories " + file);
+					directories.push(path);
 
-          var fs = filesRepository[stats.size];
-          if (!fs) {
-            fs = {
-              ____count : 1
-            };
-            filesRepository[stats.size] = fs;
-            fs['?'] = path;
+				} else if (stats.isFile() && stats.size) {
+					// console.log("Stat file " + file);
+					filesCount++;
 
-          } else if (fs.____count == 1) {
-            filesSHA1.push(fs['?']);
-            delete fs['?'];
-            filesSHA1.push(path);
-            fs.____count++;
+					var fs = filesRepository[stats.size];
+					if (!fs) {
+						fs = {
+							____count: 1
+						};
+						filesRepository[stats.size] = fs;
+						fs['?'] = path;
 
-          } else {
-            fs.____count++;
-            filesSHA1.push(path);
-          }
-        }
+					} else if (fs.____count == 1) {
+						filesSHA1.push(fs['?']);
+						delete fs['?'];
+						filesSHA1.push(path);
+						fs.____count++;
 
-        return callback(null);
-      });
-    }, callback);
-  });
+					} else {
+						fs.____count++;
+						filesSHA1.push(path);
+					}
+				}
+
+				return callback(null);
+			});
+		}, callback);
+	});
 
 });
 
 program.option("-d, --directory <path>", "Directory", function(p) {
-  directories.push(p);
-  return p;
+	directories.push(p);
+	return p;
 });
 
 program.option("-r, --remove", "Remove duplicate file");
@@ -127,74 +136,73 @@ program.option("--maxHashSize", "Specify the max hash size", parseInt);
 program.parse(process.argv);
 
 var intervalId = setInterval(function() {
-  console.log("Stats: Directory:" + directories.running() + "/" +
-      directories.length() + "  SHA1:" + filesSHA1.running() + "/" +
-      filesSHA1.length() + " count:" + filesCount);
+	console.log("Stats: Directory:" + directories.running() + "/" + directories.length() + "  SHA1:" +
+			filesSHA1.running() + "/" + filesSHA1.length() + " count:" + filesCount);
 }, 2000);
 
 function proc() {
-  if (directories.running() || filesSHA1.running()) {
-    return;
-  }
+	if (directories.running() || filesSHA1.running()) {
+		return;
+	}
 
-  clearInterval(intervalId);
+	clearInterval(intervalId);
 
-  for ( var size in filesRepository) {
-    // console.log("Size " + size + " bytes");
-    var hashs = filesRepository[size];
+	for ( var size in filesRepository) {
+		// console.log("Size " + size + " bytes");
+		var hashs = filesRepository[size];
 
-    for ( var h in hashs) {
-      if (h == "____count" || h == '?') {
-        continue;
-      }
+		for ( var h in hashs) {
+			if (h == "____count" || h == '?') {
+				continue;
+			}
 
-      var files = hashs[h];
-      if (files.length < 2) {
-        continue;
-      }
+			var files = hashs[h];
+			if (files.length < 2) {
+				continue;
+			}
 
-      console.log("Hash " + h);
+			console.log("Hash " + h);
 
-      files.forEach(function(f) {
-        console.log("  " + f);
-      });
+			files.forEach(function(f) {
+				console.log("  " + f);
+			});
 
-      if (program.removeAllo) {
-        var keep = null;
-        var sameName = true;
-        files.forEach(function(f) {
-          if (f.indexOf("__AF") >= 0 || f.indexOf("__AS") >= 0) {
-            if (keep && keep.length > f.length) {
-              return;
-            }
-            keep = f;
-          }
+			if (program.removeAllo) {
+				var keep = null;
+				var sameName = true;
+				files.forEach(function(f) {
+					if (f.indexOf("__AF") >= 0 || f.indexOf("__AS") >= 0) {
+						if (keep && keep.length > f.length) {
+							return;
+						}
+						keep = f;
+					}
 
-          if (Path.basename(f) != Path.basename(files[0])) {
-            sameName = false;
-          }
-        });
-        if (!keep && sameName) {
-          keep = files[0];
-        }
+					if (Path.basename(f) != Path.basename(files[0])) {
+						sameName = false;
+					}
+				});
+				if (!keep && sameName) {
+					keep = files[0];
+				}
 
-        if (keep) {
-          files.forEach(function(f) {
-            if (f === keep) {
-              return;
-            }
-            console.log("Remove " + f);
-            fs.unlink(f, function(error) {
-              if (error) {
-                console.log("Can not remove " + f + " : " + error);
-                return;
-              }
-            });
-          });
-        }
-      }
-    }
-  }
+				if (keep) {
+					files.forEach(function(f) {
+						if (f === keep) {
+							return;
+						}
+						console.log("Remove " + f);
+						fs.unlink(f, function(error) {
+							if (error) {
+								console.log("Can not remove " + f + " : " + error);
+								return;
+							}
+						});
+					});
+				}
+			}
+		}
+	}
 }
 
 directories.drain = proc;
